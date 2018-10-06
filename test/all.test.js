405b56bda1c66793074c1d7f6208abaa499816ee
Joël Tankam <joel.tankam@gmail.com>
2018-10-06 20:39:48 +0100

    Fix linebreak problems

--- test/all.test.js
+++ test/all.test.js
@@ -12,13 +12,13 @@ describe('matchesAll (value)', () => {
     });
 
     test('returns true on matching object', () => {
-        expect(all({a:0, b:0, c:0, d:0}, 0)).toBeTruthy();
+        expect(all({ a: 0, b: 0, c: 0, d: 0 }, 0)).toBeTruthy();
     });
     test('returns false on mismatching object', () => {
-        expect(all({a:0, b:1, c:2, d:3}, 0)).toBeFalsy();
+        expect(all({ a: 0, b: 1, c: 2, d: 3 }, 0)).toBeFalsy();
     });
     test('returns false on mismatching object', () => {
-        expect(all({a:0, b:'0'}, 0)).toBeFalsy();
+        expect(all({ a: 0, b: '0' }, 0)).toBeFalsy();
     });
 });
 
@@ -34,12 +34,12 @@ describe('matchesAll (function)', () => {
     });
 
     test('returns true on matching object', () => {
-        expect(all({a:0, b:0, c:0, d:0}, x => x === 0)).toBeTruthy();
+        expect(all({ a: 0, b: 0, c: 0, d: 0 }, x => x === 0)).toBeTruthy();
     });
     test('returns false on mismatching object', () => {
-        expect(all({a:0, b:1, c:2, d:3}, x => x === 0)).toBeFalsy();
+        expect(all({ a: 0, b: 1, c: 2, d: 3 }, x => x === 0)).toBeFalsy();
     });
     test('returns false on mismatching object', () => {
-        expect(all({a:0, b:'0'}, x => x === 0)).toBeFalsy();
+        expect(all({ a: 0, b: '0' }, x => x === 0)).toBeFalsy();
     });
 });
