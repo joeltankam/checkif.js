9b0261a0f68da232145994d34eba3895d798275d
Joël TANKAM <joel.tankam@gmail.com>
2018-10-13 20:28:28 +0100

    Add support for 'atLeast' and 'atMost' (#44)
    * Add support for 'atLeast'
    
    * Add support for 'atMost'

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 export { default as is } from './src/is';
 export { default as all } from './src/all';
 export { default as any } from './src/any';
+export { atLeast, atMost } from './src/count';
--- src/any.js
+++ src/any.js
@@ -1,23 +1,11 @@
-import { isArray, isFunction, isObject } from './is/types';
+import { atLeast } from './count';
 
 /**
  * Returns whether all values in an array/object matches
  * @param {*} enumerable the array or object to check
- * @param {*} matcher the value or function to match*
+ * @param {*} matcher the value or function to match
  * @param {Boolean} strict determines if all elements should strictly match the matcher value
  */
 export default function any(enumerable, matcher, strict = false) {
-    let matcherFunction = matcher;
-    if (strict === true || !isFunction(matcher)) matcherFunction = x => x === matcher;
-    if (isArray(enumerable)) {
-        for (let i = 0; i < enumerable.length; i += 1) {
-            if (matcherFunction(enumerable[i])) return true;
-        }
-        return false;
-    }
-    if (isObject(enumerable)) {
-        return any(Object.values(enumerable), matcherFunction);
-    }
-
-    return false;
+    return atLeast(enumerable, matcher, 1, strict);
 }
